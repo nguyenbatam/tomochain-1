77d3ffcd567c9b88028eb2f6e6fed106cc2353a7
NguyenNguyen <nguyenbk92@gmail.com>
2020-03-05 16:57:52 +0700

    tomoxlending: adding a flag to opt-in / opt-out auto top-up

--- core/types/lending_signing.go
+++ core/types/lending_signing.go
@@ -126,6 +126,13 @@ func (lendingsign LendingTxSigner) LendingCreateHash(tx *LendingTransaction) com
 	sha.Write([]byte(tx.Type()))
 	sha.Write(common.BigToHash(big.NewInt(int64(tx.Nonce()))).Bytes())
 	sha.Write(common.BigToHash(big.NewInt(int64(tx.LendingTradeId()))).Bytes())
+	if tx.Side() == LendingSideBorrow {
+		autoTopUp := int64(0)
+		if tx.AutoTopUp() {
+			autoTopUp = int64(1)
+		}
+		sha.Write(common.BigToHash(big.NewInt(autoTopUp)).Bytes())
+	}
 	return common.BytesToHash(sha.Sum(nil))
 }
 
